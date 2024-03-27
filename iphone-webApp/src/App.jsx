877311d5b9c758692ce4c1fa40f127138c7cec1c
abhay-on-it <abhayagnihotri1585@gmail.com>
2024-03-27 13:51:51 +0530

    added hero componnt

--- iphone-webApp/src/App.jsx
+++ iphone-webApp/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Components/Nav'
+import Hero from './Components/Hero'
 
 function App() {
   return (
     <div>
       <Nav/>
+      <Hero/>
     </div>
   )
 }
--- iphone-webApp/src/Components/Hero.jsx
+++ iphone-webApp/src/Components/Hero.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
+// gsap imports
+import gsap from 'gsap'
+import {useGSAP} from '@gsap/react'
+// utils imports
+import { heroVideo, smallHeroVideo } from '../utils'
 
 const Hero = () => {
+  const [videosrc, setvideosrc] = useState(window.innerWidth < 760 ? smallHeroVideo :heroVideo)
+  const handleVideoSrcSet = ()=>{
+    window.innerWidth < 760 ? setvideosrc(smallHeroVideo) : setvideosrc(heroVideo);
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize',handleVideoSrcSet);
+    return ()=>{
+      window.removeEventListener('resize',handleVideoSrcSet);
+    }
+  },[])
+  
+  useGSAP(()=>{
+     gsap.to("#hero",{opacity:1,delay:1.5})
+  },[])
   return (
-    <div>Hero</div>
+    <section className='w-full nav-height relative'>
+      <div className='w-full h-5/6 flex-center flex-col'>
+        <p id='hero' className='hero-title'>iPhone 15</p>
+        <div className='md:w-10/12 w-9/12 '>
+          <video className='pointer-events-none' autoPlay muted playsInline={true} key={videosrc} src={videosrc}></video>
+        </div>
+      </div>
+      
+    </section>
   )
 }
 
